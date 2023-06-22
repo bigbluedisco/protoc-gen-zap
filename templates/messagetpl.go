d53c8d0c4b5c6c4c14696719c107f50ad84c74ce
Mathias Griffe <mathias@bigblue.io>
2023-06-22 18:05:37 +0200

    fix/prevent-nil-pointer-exception (#5)
    Prevent nil pointer exceptions by implementing MarshalLogObject on pointers, not values, and doing a nil check first.

--- templates/messagetpl.go
+++ templates/messagetpl.go
@@ -3,7 +3,10 @@ package templates
 const messageTpl = `
 
 // MarshalLogObject makes {{ go_type_name . }} implement zap.ObjectMarshaler
-func (c {{ go_type_name . }}) MarshalLogObject(o zapcore.ObjectEncoder) error {
+func (c *{{ go_type_name . }}) MarshalLogObject(o zapcore.ObjectEncoder) error {
+	if c == nil {
+		return nil
+	}
 
 	{{ range .Fields }}
 		
