9aaa37f50b1bff9c15c424851dab60f08ff29442
Mathias Griffe <18528990+MathiasGr@users.noreply.github.com>
2024-05-21 13:56:16 +0200

    feature/migrate-to-protogen
    Migrate from pgs to protogen.

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"github.com/bigbluedisco/protoc-gen-zap/module"
-	pgs "github.com/lyft/protoc-gen-star"
-	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 )
 
 func main() {
-	pgs.
-		Init(pgs.DebugEnv("DEBUG_PGV")).
-		RegisterModule(module.Zap()).
-		RegisterPostProcessor(pgsgo.GoFmt()).
-		Render()
+	module.Run()
 }
